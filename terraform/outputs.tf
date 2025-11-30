25264504ecb4678b13aa929434d768baa2bb2c28
Neil Gebhard <neilgebhard@gmail.com>
2025-11-30 12:21:47 -0700

    Add an outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,29 @@ output "environment" {
   description = "Environment name"
   value       = var.environment
 }
+
+# VPC Outputs
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "lambda_security_group_id" {
+  description = "Lambda security group ID"
+  value       = aws_security_group.lambda.id
+}
+
+output "rds_security_group_id" {
+  description = "RDS security group ID"
+  value       = aws_security_group.rds.id
+}
